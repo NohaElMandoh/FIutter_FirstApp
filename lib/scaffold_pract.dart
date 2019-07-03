8445d66ce5fba4f2f866a4b20ad4886ab8d1d2be
NohaElMandoh <noha_m_elmandoh@outlook.com>
2019-07-03 15:34:59 -0700

    drawer And endDrawer

--- lib/scaffold_pract.dart
+++ lib/scaffold_pract.dart
@@ -3,6 +3,52 @@ class scafPract extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+//      drawer: new Drawer(child: new ListView(children: <Widget>[
+//        new DrawerHeader(child: new Text("Noha Mohammed")),
+//        new ListTile(
+//        subtitle: new Text("using dart"),
+//          onTap: (){},
+//          leading: new Icon(Icons.home),
+//          trailing: new Switch(value: true, onChanged: null),
+//        title: new Text("Flutter"),),
+//        new ListTile(
+//          selected: true,
+//    subtitle: new Text("using dart"),
+//    onTap: (){},
+//    leading: new Icon(Icons.home),
+//    trailing: new Switch(value: true, onChanged: null),
+//    title: new Text("Flutter"),),
+//        new AboutListTile(aboutBoxChildren: <Widget>[
+//          new Text("It's free")
+//        ],
+//          icon: new Icon(Icons.info),
+//          child: new Text("My First Flutter App"),)
+//
+//
+//      ],),),
+      endDrawer: new Drawer(child: new ListView(children: <Widget>[
+        new DrawerHeader(child: new Text("Noha Mohammed")),
+        new ListTile(
+          subtitle: new Text("using dart"),
+          onTap: (){},
+          leading: new Icon(Icons.home),
+          trailing: new Switch(value: true, onChanged: null),
+          title: new Text("Flutter"),),
+        new ListTile(
+          selected: true,
+          subtitle: new Text("using dart"),
+          onTap: (){},
+          leading: new Icon(Icons.home),
+          trailing: new Switch(value: true, onChanged: null),
+          title: new Text("Flutter"),),
+        new AboutListTile(aboutBoxChildren: <Widget>[
+          new Text("It's free")
+        ],
+          icon: new Icon(Icons.info),
+          child: new Text("My First Flutter App"),)
+
+
+      ],),),
       appBar: new AppBar(title: new Text("Scaffold Demo "),),
       backgroundColor: Colors.deepPurpleAccent,
       floatingActionButton: new FloatingActionButton(onPressed: null),
@@ -18,17 +64,17 @@ class scafPract extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: new BottomNavigationBar(items: <BottomNavigationBarItem>[
-        new BottomNavigationBarItem(icon:new Icon(  Icons.home),title: new Text("Home")),
-        new BottomNavigationBarItem(icon:new Icon(  Icons.add),title: new Text("Add")),
-        new BottomNavigationBarItem(icon:new Icon(  Icons.info),title: new Text("Info")),
-      ]),
-
-      persistentFooterButtons: <Widget>[
-        new FlatButton(onPressed: null, child: new Text("one")),
-        new FlatButton(onPressed: null, child: new Text("two")),
-        new FlatButton(onPressed: null, child: new Text("three")),
-      ],
+//      bottomNavigationBar: new BottomNavigationBar(items: <BottomNavigationBarItem>[
+//        new BottomNavigationBarItem(icon:new Icon(  Icons.home),title: new Text("Home")),
+//        new BottomNavigationBarItem(icon:new Icon(  Icons.add),title: new Text("Add")),
+//        new BottomNavigationBarItem(icon:new Icon(  Icons.info),title: new Text("Info")),
+//      ]),
+//
+//      persistentFooterButtons: <Widget>[
+//        new FlatButton(onPressed: null, child: new Text("one")),
+//        new FlatButton(onPressed: null, child: new Text("two")),
+//        new FlatButton(onPressed: null, child: new Text("three")),
+//      ],
       
     );
   }
